3bfe1b3ff559e0ccde5eb34d74d3f08a28d29d84
peterjsadowski <peterjsadowski@gmail.com>
2014-04-14 12:12:11 -0700

    Updated script to make predictions automatically.

--- pylearn2/scripts/improver2013/addgauss02_width1000.py
+++ pylearn2/scripts/improver2013/addgauss02_width1000.py
@@ -99,8 +99,45 @@ def train(mytrain):
     sys.stdout = open(logfile, 'w')        
     mytrain.main_loop()
 
+def predict(model):
+    '''
+    Make predictions from model.
+    '''
+    import os
+    os.environ['THEANO_FLAGS'] = "mode=FAST_RUN,device=cpu,floatX=float32" # gpu0,etc.
+    import theano
+    print 'Using: %s' % theano.config.device
+    import pylearn2
+    import pickle as pkl
+    from pylearn2.config import yaml_parse
+    import numpy as np
+    import pylearn2.datasets.improver2013 as improver2013
+    import pylab
+    import pylearn2.utils.dropoutsimulator as simulator
+    #model = pkl.load(open('addgauss02_width1000.pkl', 'r'))
+
+    # Load train and test data.
+    traindata = improver2013.IMPROVER2013(which_set='A_phospho', binarize=True, randomize=False, holdout=False)
+    data = improver2013.IMPROVER2013(which_set='B_phospho', binarize=True, randomize=False, holdout=False)
+    # Predict on train data.
+    trainpred = simulator.simulate(model, dataset=traindata)
+    trainpred = trainpred[-1][0,:,:].T
+    # Predict on test data.
+    testpred = simulator.simulate(model, dataset=data)
+    testpred = testpred[-1][0,:,:].T
+
+    # Save network input
+    np.savetxt('GEx_rat_train.txt.output', traindata.X, fmt='%0.12f') 
+    np.savetxt('GEx_rat_test.txt.output', data.X, fmt='%0.12f') 
+    # Save network output
+    np.savetxt('GEx_human_train.txt.output', traindata.y, fmt='%0.12f') 
+    np.savetxt('GEx_human_train.txt.predicted', trainpred, fmt='%0.12f')
+    np.savetxt('GEx_human_test.txt.predicted', testpred, fmt='%0.12f') 
+
 
 if __name__=='__main__':
-    # Initialize and train.
+    # Initialize, train, predict.
     mytrain = init_train()
     train(mytrain) 
+    predict(mytrain.model)
+
